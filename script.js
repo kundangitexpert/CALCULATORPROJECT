72514ddabbcd4c016c81137ee046c507f5862e11
kundangitexpert <157510851+kundangitexpert@users.noreply.github.com>
2024-02-23 19:32:48 +0530

    Add files via upload
    i have added my all code of project thats all .

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let display = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
+// console.log(buttons)
+
+let buttonsarray = Array.from(buttons);
+let string = ' ';
+buttonsarray.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = ' ';
+            display.value = string;
+        }
+        else if(e.target.innerHTML == '=')
+        {
+            string=eval(string);
+            display.value=string;
+        }
+        else {
+            string += e.target.innerHTML;
+            display.value = string;
+        }
+
+
+
+
+
+    });
+
+
+
+});
